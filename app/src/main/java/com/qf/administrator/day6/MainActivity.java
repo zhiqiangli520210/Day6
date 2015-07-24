7316718341d8fd1e4be550efedcfd784786dd268
aaa <123>
2015-07-24 08:44:17 +0800

    adff

--- app/src/main/java/com/qf/administrator/day6/MainActivity.java
+++ app/src/main/java/com/qf/administrator/day6/MainActivity.java
@@ -8,6 +8,9 @@ import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
 
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
